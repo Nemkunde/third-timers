0759782fd0061a67fa396479e4c4db509dd73872
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2023-05-25 11:52:02 +0200

    adminbookpage changes

--- src/components/addBooksBtn.js
+++ src/components/addBooksBtn.js
@@ -1,1 +1,8 @@
 //a button to add a new book
+import React from "react";
+
+function addBooksBtn() {
+  return <div>addBooksBtn</div>;
+}
+
+export default addBooksBtn;
--- src/pages/adminBookPage.js
+++ src/pages/adminBookPage.js
@@ -1,17 +1,20 @@
-/*This file will render an admin view with all available book categories a search bar, a sign out button, and five different buttons for the order/action categories of the table: 
-increment/decrement, order, edit and delete button. There will also be a tab for changing tables for user and book view.*/
 import React from "react";
 
-//Only hardcoding the static data for structure.
-function adminBookPage() {
+function AdminBookPage() {
+  const handleAddbtn = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="adminBookPage-container">
       <input
         type="text"
         placeholder="Search query..."
         name="searchInput"
+        autoComplete="off"
       ></input>
-      <button>Add new book</button>
+      <button type="submit" onClick={handleAddbtn}>
+        Add new book
+      </button>
       <table>
         <thead>
           <tr>
@@ -26,10 +29,20 @@ function adminBookPage() {
             <td>Lorem Ipsum</td>
             <td>2 left</td>
           </tr>
+          <tr>
+            <td>Lorem Ipsum</td>
+            <td>Lorem Ipsum</td>
+            <td>2 left</td>
+          </tr>
+          <tr>
+            <td>Lorem Ipsum</td>
+            <td>Lorem Ipsum</td>
+            <td>2 left</td>
+          </tr>
         </tbody>
       </table>
     </div>
   );
 }
 
-export default adminBookPage;
+export default AdminBookPage;
